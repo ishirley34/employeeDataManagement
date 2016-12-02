e995c16e9fbc9bd2ec3c211dfd3b403e4ec8dc8b
drichard1989 <drichard1989@icloud.com>
2016-12-01 19:58:09 -0600

    Added content to the JS file

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,6 @@
-  // Initialize Firebase
-  var config = {
+$(document).ready(function(){
+
+var config = {
     apiKey: "AIzaSyAHklHQGal5D9D-vutKcBxKjxdVvT5knTE",
     authDomain: "employeedatamanagement-1f7a3.firebaseapp.com",
     databaseURL: "https://employeedatamanagement-1f7a3.firebaseio.com",
@@ -8,5 +9,24 @@
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
 
+  var name = "";
+  var email = "";
+  var age = 0;
+  var comment = "";
+
+
+  $("#")
+
+
+
+
+
+
+
+
+
+
+});  
   //  HEre are some weird changes
