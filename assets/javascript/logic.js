eb633c123b9c26012b9fb4eed11d93fa5a583c55
ian shirley <ian shirley>
2016-12-01 19:30:54 -0600

    firebase code to JS

---
+++ assets/javascript/logic.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAHklHQGal5D9D-vutKcBxKjxdVvT5knTE",
+    authDomain: "employeedatamanagement-1f7a3.firebaseapp.com",
+    databaseURL: "https://employeedatamanagement-1f7a3.firebaseio.com",
+    storageBucket: "employeedatamanagement-1f7a3.appspot.com",
+    messagingSenderId: "813836623686"
+  };
+  firebase.initializeApp(config);
